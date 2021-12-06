decf7d2dc11ce733e8be45f143338034a1f267d8
LeoToti <Londra27?>
2021-12-06 14:47:52 +0100

    App

---
+++ src/components/Home.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import axios from "axios";
+import Profiles from "./Profiles";
+import ProfilesFunctional from "./ProfileFunctional"
+import { Row, Div, Col } from "react-bootstrap";
+
+class Home extends React.Component {
+  state = {
+    queryError: "",
+    selected: [],
+    show: "",
+  };
+
+  fectchData = async function () {
+    this.setState({ isLoading: true });
+    let endpoint = "https://randomuser.me/api";
+
+    try {
+      let response = await fetch(endpoint);
+      if (response.ok) {
+        console.log("Response is ok!!");
+        let data = await response.json();
+        console.log(data);
+        if (data) {
+          this.setState({ selected: data.results });
+          console.log(this.state.selected);
+        } else {
+          this.setState({ queryError: data.Error });
+          console.log(this.state.queryError);
+        }
+      } else {
+        alert("Cant fetch the data!");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  componentDidMount = async function () {
+    this.fectchData();
+  };
+
+  render() {
+    return (
+      <>
+        {/* <Profiles user={this.state.selected} /> */}
+        <ProfilesFunctional data={this.state.selected} />
+      </>
+    );
+  }
+}
+
+export default Home;
---
+++ src/components/ProfileFunctional.jsx
@@ -0,0 +1,130 @@
+import { React, useState } from "react";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import { HiChevronDoubleDown } from "react-icons/hi";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { AiFillPhone, AiOutlineUser, AiTwotoneMail } from "react-icons/ai";
+import { FaAddressBook, FaBirthdayCake } from "react-icons/fa";
+import Datetime from "react-datetime";
+
+const ProfileFunctional = ({ data }) => {
+  const [show, setShow] = useState("");
+  const [field, setField] = useState("");
+
+  return (
+    <div>
+      {data.map((users) => (
+        <Container key={users.login.uuid}>
+          {users.picture != null ? (
+            <Row className="mb-4 mt-2 justify-content-center">
+              <Col xs={6} md={4}>
+                <Image src={users.picture.large} roundedCircle />
+              </Col>
+            </Row>
+          ) : (
+            <Row className="mb-4">
+              <Col xs={6} md={4}>
+                <Image src="holder.js/171x180" roundedCircle />
+              </Col>
+            </Row>
+          )}
+          <Row className="mb-4">
+            <p>{field}</p>
+            <h3>{show}</h3>
+          </Row>
+          <Row>
+            <Col>
+              <RiLockPasswordFill
+                onMouseOver={({ target }) => {
+                  target.style.color = "green";
+                  setShow(users.login.password);
+                  setField("My Password is");
+                }}
+                onMouseOut={({ target }) => {
+                  target.style.color = "black";
+                }}
+                size={60}
+              />
+            </Col>
+            <Col>
+              <AiFillPhone
+                onMouseOver={({ target }) => {
+                  target.style.color = "green";
+                  setShow(users.phone);
+                  setField("My phone number is");
+                }}
+                onMouseOut={({ target }) => {
+                  target.style.color = "black";
+                }}
+                size={60}
+              />
+            </Col>
+            <Col>
+              <FaAddressBook
+                onMouseOver={({ target }) => {
+                  target.style.color = "green";
+                  setShow(
+                    users.location.street.name +
+                      " " +
+                      users.location.street.number
+                  );
+                  setField("My address is");
+                }}
+                size={60}
+                onMouseOut={({ target }) => {
+                  target.style.color = "black";
+                }}
+              />
+            </Col>
+            <Col>
+              <FaBirthdayCake
+                onMouseOver={({target}) => {
+                    target.style.color = "green";
+                    setShow( users.dob.date ) ;
+                    setField("My birthday is")}}
+
+                    size={60}
+                    onMouseOut={({ target }) => {
+                        target.style.color = "black";}}
+                    
+              />
+            </Col>
+            <Col>
+              <AiOutlineUser
+                onMouseOver={({ target }) => {
+                  target.style.color = "green";
+                  setShow(
+                    users.name.title +
+                      " " +
+                      users.name.first +
+                      " " +
+                      users.name.last
+                  );
+                  setField("My name is");
+                }}
+                size={60}
+                onMouseOut={({ target }) => {
+                  target.style.color = "black";
+                }}
+              />
+            </Col>
+            <Col>
+              <AiTwotoneMail
+                onMouseOver={({ target }) => {
+                  target.style.color = "green";
+                  setShow(users.email);
+                  setField("My email is");
+                }}
+                size={60}
+                onMouseOut={({ target }) => {
+                  target.style.color = "black";
+                }}
+              />
+            </Col>
+          </Row>
+        </Container>
+      ))}
+    </div>
+  );
+};
+
+export default ProfileFunctional;
